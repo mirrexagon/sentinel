d568be00c9f535ad3a07cb0a819172b6cde2dbbf
Andrew Abbott <mirrexagon@mirrexagon.com>
2018-08-08 13:30:25 +0000

    Add TODO about periodic saving

--- src/modules/talklike.rs
+++ src/modules/talklike.rs
@@ -1,5 +1,5 @@
 // Ideas:
-// - Ignore commands.
+// - Save periodically. Do this for all modules in main?
 // - Make it per user per server? Users would expect it to keep
 // what they say per server.
 
