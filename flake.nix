6b10cee5f47c14d04f9da5053c457f5f20f89c29
Andrew Abbott <mirrexagon@mirrexagon.com>
2021-04-17 11:55:34 +1000

    Use default Rust

--- flake.nix
+++ flake.nix
@@ -5,39 +5,31 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    utils.url = "github:numtide/flake-utils";
-    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+
     crate2nix = {
       url = "github:kolloch/crate2nix";
       flake = false;
     };
+
     flake-compat = {
       url = "github:edolstra/flake-compat";
       flake = false;
     };
   };
 
-  outputs = { self, nixpkgs, utils, rust-overlay, crate2nix, ... }:
+  outputs = { self, nixpkgs, flake-utils, crate2nix, ... }:
     let
       name = "sentinel";
     in
-    utils.lib.eachDefaultSystem
+    flake-utils.lib.eachDefaultSystem
       (system:
         let
           # Imports
           pkgs = import nixpkgs {
             inherit system;
-            overlays = [
-              rust-overlay.overlay
-              (self: super: {
-                # Because rust-overlay bundles multiple rust packages into one
-                # derivation, specify that mega-bundle here, so that crate2nix
-                # will use them automatically.
-                rustc = self.rust-bin.stable.latest.default;
-                cargo = self.rust-bin.stable.latest.default;
-              })
-            ];
           };
+
           inherit (import "${crate2nix}/tools.nix" { inherit pkgs; })
             generatedCargoNix;
 
@@ -73,7 +65,7 @@
           defaultPackage = packages.${name};
 
           # `nix run`
-          apps.${name} = utils.lib.mkApp {
+          apps.${name} = flake-utils.lib.mkApp {
             inherit name;
             drv = packages.${name};
           };
