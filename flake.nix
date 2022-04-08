3db017b5f41d1890afd7ca3d0d3d21743bab59b9
Andrew Abbott <andrew@mirx.dev>
2022-04-09 09:16:12 +1000

    Update deprecated flake output attributes

--- flake.nix
+++ flake.nix
@@ -12,33 +12,36 @@
   };
 
   outputs = { self, nixpkgs, utils, naersk, ... }:
-  let
+    let
       pname = "sentinel";
-  in {
-    overlay = final: prev: {
-      "${pname}" = naersk.lib."${final.system}".buildPackage {
-        inherit pname;
-        root = ./.;
+    in
+    {
+      overlays.default = final: prev: {
+        "${pname}" = naersk.lib."${final.system}".buildPackage {
+          inherit pname;
+          root = ./.;
+        };
       };
-    };
-  } // (
-    utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs {
-        inherit system;
-        overlays = [ self.overlay ];
-      };
-    in rec {
-      packages."${pname}" = pkgs."${pname}";
-      defaultPackage = packages."${pname}";
+    } // (
+      utils.lib.eachDefaultSystem (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ self.overlays.default ];
+          };
+        in
+        rec {
+          packages."${pname}" = pkgs."${pname}";
+          packages.default = packages."${pname}";
 
-      apps."${pname}" = utils.lib.mkApp {
-        drv = packages."${pname}";
-      };
-      defaultApp = apps."${pname}";
+          apps."${pname}" = utils.lib.mkApp {
+            drv = packages."${pname}";
+          };
+          defaultApp = apps."${pname}";
 
-      devShell = pkgs.mkShell {
-        nativeBuildInputs = with pkgs; [ rustc cargo ];
-      };
-    })
-  );
+          devShells.default = pkgs.mkShell {
+            nativeBuildInputs = with pkgs; [ rustc cargo ];
+          };
+        })
+    );
 }
